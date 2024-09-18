158c654cd5cd2c2efe0b684a01592c260addc0c4
Geiziane Silva <4663072@alunouninter.com>
2024-09-18 13:48:13 -0300

    :boom: mudança na variável do env

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-require('dotenv').config(MONGO_URL);
+require('dotenv').config(MONGODB_URL);
 
 
 async function conectaBancoDeDados() {
     try {
         console.log('A conexão com o banco de dados iniciou');
 
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGODB_URL);
 
         console.log('A conexão com o banco de dados foi feita com sucesso');
     } catch(erro) {
